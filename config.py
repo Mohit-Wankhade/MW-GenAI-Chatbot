d60b907a7a07d823c8e7171912bd33be94de364a
Mohit-Wankhade <wmohit301@gmail.com>
2026-07-03 23:20:12 +0530

    Add default environment values for CI

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-MONGO_URL = os.getenv("MONGO_URL")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
+MONGO_URL = os.getenv("MONGO_URL", "")
 
 PDF_INDEX_PATH = "storage/pdf_index"
 GITHUB_INDEX_PATH = "storage/github_index"
@@ -15,10 +15,9 @@ GITHUB_CHUNK_PATH = "storage/github_chunks.pkl"
 UPLOAD_FOLDER = "storage/uploads"
 REPO_FOLDER = "repos"
 
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = int(os.getenv("REDIS_PORT"))
-REDIS_DB = int(os.getenv("REDIS_DB"))
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_DB = int(os.getenv("REDIS_DB", "0"))
 
-
-SECRET_KEY= "CHATBOT_KEY"
+SECRET_KEY = os.getenv("SECRET_KEY", "CHATBOT_KEY")
 ALGORITHM = "HS256"
